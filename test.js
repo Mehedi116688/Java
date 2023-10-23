d298793daf56a8f0009cb96195f3380ae5b5f40c
Mehedi116688 <101993545+Mehedi116688@users.noreply.github.com>
2023-10-23 22:08:41 +0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,139 @@
+// => Solve Those Problems 
+
+// 1. Declare a variable called name and assign your name to it.
+// let name = "Mehedi";
+// console.log(name);
+
+// 2. Create a variable age and assign your age to it.
+// let age = 32;
+// console.log(age);
+
+// 3. Calculate the sum of two numbers, num1 and num2.
+// let num1 = 10;
+// let num2 = 20;
+// sum = num1 + num2;
+
+// console.log(sum);
+
+// console.log(num1 + num2);
+
+// 4. Calculate the product of two numbers, num1 and num2.
+// let num1 = 5;
+// let num2 = 15;
+// sum = num1 + num2;
+
+// console.log(sum);
+
+// 5. Check if a number is even or odd. Create a variable isEven and assign true if the number is even, and false if it's odd.
+// let isEven = 49;
+
+// if (isEven%2 == 0) {
+//     console.log('true');
+// } else {
+//     console.log('false');
+    
+// }
+
+
+// 6. Create an array favoriteColors containing three favorite colors of yours.
+
+// let favoriteColors = ['red', 'black', 'wehite'];
+
+// console.log(favoriteColors);
+
+// 7. Access and log the 2nd favorite color from the favoriteColors array.
+// let favoriteColors = ['red', 'black', 'wehite'];
+// console.log(favoriteColors[1]);
+
+
+// 8. Add a new color, "purple," to the favoriteColors array.
+// let favoriteColors = ['red', 'black', 'wehite'];
+// favoriteColors.push('purple');
+// console.log(favoriteColors);
+
+// 9. Check if a given number is greater than 10. If it is, log "Greater than 10," otherwise, log "Less than or equal to 10."
+
+// let number = 5;
+
+// if (number >= 10) {
+//     console.log('Greater than 10');
+// } else {
+//     console.log('Less than or equal to 10');
+// }
+
+// 10. Create a variable isRaining and assign true if it's raining, and false if it's not.
+
+// let isRaining = true;
+
+// if (isRaining) {
+//     console.log('it is raining');
+// } else {
+//     console.log('it is not raining');
+// }
+
+
+
+// 11. Create a variable temperature and assign a value. Then, check if it's hot (greater than 30), warm (between 20 and 30), or cold (less than 20).
+
+// let temperature = 18;
+
+// if (temperature < 20) {
+//     console.log('cold');
+// } else if(temperature > 20 && temperature < 30){
+//     console.log('warm');
+// }else{
+//     console.log('Hot');
+// }
+
+
+// 12. Calculate the area of a rectangle with a given width and height.
+
+// let width = 20;
+// height = 10;
+
+// console.log(width*height);
+
+// 13. Create an array favoriteFruits containing five favorite fruits of yours.
+
+// let favoriteFruits = ['Orange', 'apple', 'mango', 'pinaple', 'Kiwi'];
+
+// console.log(favoriteFruits);
+
+// 14. Access and log the 3rd favorite fruit from the favoriteFruits array.
+// let favoriteFruits = ['Orange', 'apple', 'mango', 'pinaple', 'Kiwi'];
+// console.log(favoriteFruits[2]);
+
+// 15. Add a new favorite fruit, "grape," to the favoriteFruits array.
+// let favoriteFruits = ['Orange', 'apple', 'mango', 'pinaple', 'Kiwi'];
+
+// favoriteFruits.push('grape');
+
+// console.log(favoriteFruits);
+
+
+// 16. Check if a student's score is greater than or equal to 70. If it is, log "Pass," otherwise, log "Fail."
+
+// let score = 69;
+// let student;
+
+// if (score > 70) {
+//     student = "pass";
+    
+// }else{
+//     student = "fail";
+    
+// }
+
+// console.log(student);
+
+
+// 17. Create an array of three favorite movies.
+
+// let favoriteMovie = ['3idots', 'titanic', 'kgf'];
+// console.log(favoriteMovie);
+
+// 18. Access and log the 2nd favorite movie from the favoriteMovies array.
+
+// let favoriteMovie = ['3idots', 'titanic', 'kgf'];
+// console.log(favoriteMovie[1]);
+  
